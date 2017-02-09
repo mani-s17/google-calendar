4e4034d419a28fc1b8f40dcc0d0de276423c0989
Subramaniam S <subramaniam.s17@gmail.com>
2017-02-10 00:22:53 +0530

    base index file implementation with angular & material angular

---
+++ src/app.module.js
@@ -0,0 +1,17 @@
+/**
+ * Created by subramaniam on 09/02/17.
+ */
+
+var app = angular.module("calendarApp", ['ngRoute', 'ngMaterial', 'ngMessages', 'ngAnimate']);
+
+app.config(['$routeProvider', '$locationProvider', '$mdThemingProvider',
+  function ($routeProvider, $locationProvider, $mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('green');
+
+    $routeProvider.when('/', {
+      templateUrl: 'src/module/login/login.html'
+    });
+
+    $locationProvider.html5Mode(true);
+  }]);
